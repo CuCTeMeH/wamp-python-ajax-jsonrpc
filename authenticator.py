4965a78216cb1f99dbc44e0599a0ef7f86c96d4e
CuCTeMeH <kutinchev87@gmail.com>
2017-03-16 15:23:21 +0200

    - Fetching the cookies in the backend component so we can send them to the ajax url together with the parameters.

--- authenticator.py
+++ authenticator.py
@@ -2,6 +2,8 @@ from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner, Applica
 from autobahn import wamp
 from twisted.internet.defer import inlineCallbacks
 from twisted.python.failure import Failure
+import requests
+import json
 
 
 class PandaXAuthenticator(ApplicationSession):
@@ -23,15 +25,24 @@ class PandaXAuthenticator(ApplicationSession):
     @wamp.register(u'call.rest.authenticate')
     def authenticate(self, realm, authid, details):
         print("WAMP-CRA dynamic authenticator invoked: realm='{}', authid='{}'".format(realm, authid))
-        print(details)
+        # print(details)
+        #
+        cookies = details.get('transport', {}).get('http_headers_received', {}).get('cookie', {})
+        response = self.get_auth_token()
+
+
+
+        # pass the cookies to the auth server so that we can check if the user is authenticated. Then return the correct fucking thing and then be done with the auth shit thingy.
+
+
         # print(self)
         # check database for user session or make a rest call to the auth api with the request cookie so that we can be sure the user is logged in.
-        if(authid == 'joe'):
-            return {
-              # these are required:
-              'secret': 'secret2',  # the secret/password to be used
-              'role': 'frontend'    # the auth role to be assigned when authentication succeeds
-            }
+        # if(authid == 'joe'):
+        return {
+            # these are required:
+            'secret': 'probid',  # the secret/password to be used
+            'role': 'frontend'    # the auth role to be assigned when authentication succeeds
+        }
         # else:
             # raise ApplicationError(u'call.rest.authenticate.no_such_user',
             #                        'could not authenticate session - no such user {}'.format(authid))
@@ -40,3 +51,15 @@ class PandaXAuthenticator(ApplicationSession):
         #     return USERDB[authid]
         # else:
         #     raise ApplicationError(u'com.example.no_such_user', 'could not authenticate session - no such user {}'.format(authid))
+
+    @staticmethod
+    def get_auth_token():
+        # headers = {'content-type': 'application/json'}
+        # payload = {
+        #     "jsonrpc": "2.0",
+        #     "id": 0,
+        # }
+        #
+        # response = requests.get('', data=json.dumps(payload), headers=headers, cookies=cookies).json()
+
+        return 123123
--- pandaxServer.py
+++ pandaxServer.py
@@ -3,21 +3,32 @@ import json
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner, ApplicationSessionFactory
 from autobahn.wamp.types import RegisterOptions
 from twisted.internet.defer import inlineCallbacks
-
+from authenticator import PandaXAuthenticator
 
 class PandaX(ApplicationSession):
     def __init__(self, config=None):
         ApplicationSession.__init__(self, config)
+        self.cookies = None
 
     def onConnect(self):
         self.join(self.config.realm)
 
     @inlineCallbacks
     def onJoin(self, details):
+        yield self.subscribe(self.on_session_join, u'wamp.session.on_join')
         yield self.register(self.jsonrpc, u"jsonrpc.", options=RegisterOptions(match=u"prefix", details_arg="details"))
+
+    def on_session_join(self, session_details):
+        # print("WAMP session has joined:")
+        # print(session_details)
+        self.cookies = session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {})
 
     def jsonrpc(self, url, method, params, publish=True, details=None):
         print(details)
+        print(params)
+        print(PandaXAuthenticator.get_auth_token())
+        print(self.cookies)
+
         procedure = details.procedure
         headers = {'content-type': 'application/json'}
         payload = {
