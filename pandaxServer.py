bb4e509b1ba0f71af7d99696c6e9b0ac030f7958
CuCTeMeH <kutinchev87@gmail.com>
2017-06-05 14:54:51 +0300

    Fixed the cookie store to use redis for dynamic cookies. Fixed a bug that kept the first cookie only. The code needs cleanup.

--- pandaxServer.py
+++ pandaxServer.py
@@ -7,6 +7,7 @@ from http.cookies import SimpleCookie
 import requests
 import json
 import urllib
+import redis
 
 
 class PandaX(ApplicationSession):
@@ -14,6 +15,9 @@ class PandaX(ApplicationSession):
         ApplicationSession.__init__(self, config)
         self.cookies = None
         self.oauthCookie = None
+        self.topics = {}
+        self.topic_ids = {}
+        self.user_sessions = {}
 
     def onConnect(self):
         self.join(self.config.realm)
@@ -21,32 +25,35 @@ class PandaX(ApplicationSession):
     @inlineCallbacks
     def onJoin(self, details):
         yield self.subscribe(self.on_session_join, u'wamp.session.on_join')
+        yield self.subscribe(self.on_subscribe, u'wamp.subscription.on_subscribe')
+        yield self.subscribe(self.on_subscribe_create, u'wamp.subscription.on_create')
         yield self.register(self.jsonrpc, u"jsonrpc.", options=RegisterOptions(match=u"prefix", details_arg="details"))
 
-    def on_session_join(self, session_details):
-        """
-        We store the cookies here in the this session variable foreach session.
-        
-        Todo: Maybe refactor this to use redis because self can be overwritten any time. But if not needed there is no need to overcomplicate stuff with adding a redis set and get and cleanup when the cookie expires.
-        
-        :param session_details: 
-        :return: 
-        """
-        # print("WAMP session has joined:")
-        # print(session_details)
-        # print(session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {}))
+    def on_subscribe_create(self, session, subscription_details):
+        topic = subscription_details['uri']
+        topic_id = subscription_details['id']
+        self.topics[topic_id] = topic
+        self.topic_ids[topic] = topic_id
 
+    def on_subscribe(self, session, subscription):
+        topic = self.topics[subscription]
+        subscribers = self.call('wamp.subscription.list_subscribers', subscription)
+        # print(topic)
+        # print(subscribers)
+
+    def get_laravel_session(self, session_details):
         cookie = SimpleCookie()
         cookie.load(session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {}))
 
         # Even though SimpleCookie is dictionary-like, it internally uses a Morsel object
         # which is incompatible with requests. Manually construct a dictionary instead.
         cookies = {}
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
         for key, morsel in cookie.items():
             if key == 'laravel_oauth_session':
-                if self.oauthCookie:
-                    morsel.value = self.oauthCookie
+                if r.exists(morsel.value):
+                    morsel.value = r.get(morsel.value).decode("utf-8")
                 else:
                     token = PandaXAuthenticator.get_auth_token()
                     # print(token)
@@ -62,15 +69,46 @@ class PandaX(ApplicationSession):
                                              data=json.dumps(payload), headers=headers).json()
 
                     if response and 'error' in response:
-                        return self.on_session_join(self, session_details)
+                        return self.get_laravel_session(self, session_details)
 
                     if response['status']:
-                        self.oauthCookie = response['cookie']
+                        r.set(morsel.value, response['cookie'])
                         morsel.value = response['cookie']
 
             cookies[key] = morsel.value
-        # print(cookies)
-        self.cookies = cookies
+            self.cookies = cookies
+        return cookies
+
+    def on_session_join(self, session_details):
+        """
+        We store the cookies here in the this session variable foreach session.
+        
+        Todo: Maybe refactor this to use redis because self can be overwritten any time. But if not needed there is no need to overcomplicate stuff with adding a redis set and get and cleanup when the cookie expires.
+        
+        :param session_details: 
+        :return: 
+        """
+        # print("WAMP session has joined:")
+        # print(session_details.get('session'))
+        # print(session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {}))
+        cookies = self.get_laravel_session(session_details)
+        session_id = session_details.get('session')
+        logged_user = PandaXAuthenticator.is_logged_in(cookies)
+        # print(logged_user)
+        logged_user_request_status = logged_user.get('status')
+        if logged_user_request_status:
+            user_id = logged_user['user']['id']
+            # print(self.user_sessions)
+            # print(session_id)
+            # if(self.user_sessions.get(user_id).get(session_id) == None):
+            if self.user_sessions.get(user_id) is None:
+                self.user_sessions[user_id] = {}
+            self.user_sessions[user_id][session_id] = session_id
+
+        # print(self.user_sessions)
+        # print(logged_user)
+        # if logged_user and 'user' in logged_user:
+        #     self.user_sessions[logged_user['user']['id']][session_id] =  session_id
 
     def jsonrpc(self, url, method, params, publish=True, details=None, recurse=True):
         """
@@ -91,8 +129,12 @@ class PandaX(ApplicationSession):
         # print(params)
         # print(PandaXAuthenticator.get_auth_token())
         # print(self.cookies)
+        # print(self.user_sessions)
         token = PandaXAuthenticator.get_auth_token()
+        topic_ids = self.topic_ids[details.procedure]
+        # print(topic_ids)
         is_logged_in = PandaXAuthenticator.is_logged_in(self.cookies)
+        # print(is_logged_in)
         response = None
 
         if is_logged_in or params['allow_anonymous']:
@@ -120,7 +162,7 @@ class PandaX(ApplicationSession):
 
                 return self.jsonrpc(self, url, method, params, publish, details, False)
 
-            if response.publish:
+            if response and 'publish' in response:
                 self.publish(procedure, response)
 
         return response
