5100d2d9d898420c668f1a5d0173f7ab3e10aee8
CuCTeMeH <kutinchev87@gmail.com>
2017-06-06 18:41:46 +0300

    Adding Events and writing to redis two objects: socket_user_sessions, socket_topics_to_users. Still needs cleanup. Will debug tomorrow.

--- pandaxServer.py
+++ pandaxServer.py
@@ -1,7 +1,7 @@
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner, ApplicationSessionFactory
 from autobahn.wamp.types import RegisterOptions
 from autobahn.wamp.exception import ApplicationError
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, returnValue
 from authenticator import PandaXAuthenticator
 from http.cookies import SimpleCookie
 import requests
@@ -18,6 +18,7 @@ class PandaX(ApplicationSession):
         self.topics = {}
         self.topic_ids = {}
         self.user_sessions = {}
+        self.topics_to_users = {}
 
     def onConnect(self):
         self.join(self.config.realm)
@@ -27,23 +28,48 @@ class PandaX(ApplicationSession):
         yield self.subscribe(self.on_session_join, u'wamp.session.on_join')
         yield self.subscribe(self.on_leave, u'wamp.session.on_leave')
         yield self.subscribe(self.on_subscribe, u'wamp.subscription.on_subscribe')
+        yield self.subscribe(self.on_unsubscribe, u'wamp.subscription.on_unsubscribe')
         yield self.subscribe(self.on_subscribe_create, u'wamp.subscription.on_create')
         yield self.register(self.jsonrpc, u"jsonrpc.", options=RegisterOptions(match=u"prefix", details_arg="details"))
+        yield self.register(self.system_private, u"system.private.", options=RegisterOptions(match=u"prefix", details_arg="details"))
 
     def on_leave(self, session):
-        # print(session)
         user_sessions = self.user_sessions
-        # print(type(user_sessions))
-        # print('LEAVING SESSION')
         for user_id, sessions in user_sessions.items():
             for session_id, s_id in sessions.copy().items():
                 if session_id == session:
                     sessions.pop(session_id)
-                    # del(user_sessions[user_id][session_id])
-            # print(v)
-        # print(user_sessions)
-        # print('LEAVING SESSION')
+                    self.del_topics_to_users(user_id)
+
         self.user_sessions = user_sessions
+        self.update_user_sessions_redis()
+        self.update_users_to_topics_redis()
+
+    def update_user_sessions_redis(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        r.set('socket_user_sessions', self.user_sessions)
+
+    def update_users_to_topics_redis(self):
+        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        r.set('socket_topics_to_users', self.topics_to_users)
+
+    def add_topics_to_users(self, topic, user):
+        topics_to_users = dict()
+        topics_to_users[topic] = user
+        self.topics_to_users = topics_to_users
+        self.update_users_to_topics_redis()
+
+    def del_topics_to_users(self, user, topic=None):
+        topics_to_users = self.topics_to_users
+        if topic is not None:
+            topics_to_users = topics_to_users.get(topic, {}).pop(user, None)
+        else:
+            for t, u in topics_to_users.copy().items():
+                if u == user:
+                    topics_to_users.pop(u, None)
+
+        self.topics_to_users = topics_to_users
+        self.update_users_to_topics_redis()
 
     def on_subscribe_create(self, session, subscription_details):
         topic = subscription_details['uri']
@@ -51,18 +77,35 @@ class PandaX(ApplicationSession):
         self.topics[topic_id] = topic
         self.topic_ids[topic] = topic_id
 
+    def on_unsubscribe(self, session, subscription):
+        topic = self.topics[subscription]
+        users = self.user_sessions
+
+        for user_id, user_socket_id in users.items():
+            for sock_ids in user_socket_id:
+                if session == sock_ids:
+                    self.del_topics_to_users(user_id, topic)
+
+    @inlineCallbacks
     def on_subscribe(self, session, subscription):
         topic = self.topics[subscription]
-        subscribers = self.call('wamp.subscription.list_subscribers', subscription)
-        # print(topic)
-        # print(subscribers)
+        users = self.user_sessions
+
+        subscribers = yield self.call('wamp.subscription.list_subscribers', subscription)
+        for subs in subscribers:
+            for user_id, user_socket_id in users.items():
+                for sock_ids in user_socket_id:
+                    if session == sock_ids == subs and topic.startswith('system.private.') and topic != 'system.private.' + str(user_id):
+                        # TODO: Here when the remove subscriber is called because it is async a frontend JS error occurs stating: "Uncaught DOMException: Failed to execute 'close' on 'WebSocket': The code must be either 1000, or between 3000 and 4999. 1002 is neither.". This only occurs when a hacker tries to subscribe to a private channel so not highest priority but investigate this when can.
+                        yield self.call("wamp.subscription.remove_subscriber", subscription_id=subscription, subscriber_id=session)
+                        return
+                    elif session == sock_ids == subs:
+                        self.add_topics_to_users(topic, user_id)
 
     def get_laravel_session(self, session_details):
         cookie = SimpleCookie()
         cookie.load(session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {}))
 
-        # Even though SimpleCookie is dictionary-like, it internally uses a Morsel object
-        # which is incompatible with requests. Manually construct a dictionary instead.
         cookies = {}
         r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -102,29 +145,21 @@ class PandaX(ApplicationSession):
         :param session_details: 
         :return: 
         """
-        # print("WAMP session has joined:")
-        # print(session_details.get('session'))
-        # print(session_details.get('transport', {}).get('http_headers_received', {}).get('cookie', {}))
         cookies = self.get_laravel_session(session_details)
         session_id = session_details.get('session')
         logged_user = PandaXAuthenticator.is_logged_in(cookies)
-        # print(logged_user)
         logged_user_request_status = logged_user.get('status')
         if logged_user_request_status:
             user_id = logged_user['user']['id']
-            # print(self.user_sessions)
-            # print(session_id)
-            # if(self.user_sessions.get(user_id).get(session_id) == None):
             if self.user_sessions.get(user_id) is None:
                 self.user_sessions[user_id] = {}
             self.user_sessions[user_id][session_id] = session_id
+        self.update_user_sessions_redis()
 
-        print(self.user_sessions)
-        # print(logged_user)
-        # if logged_user and 'user' in logged_user:
-        #     self.user_sessions[logged_user['user']['id']][session_id] =  session_id
+    def system_private(self, details=None):
+        return True
 
-    def jsonrpc(self, url, method, params, publish=True, details=None, recurse=True):
+    def jsonrpc(self, url, method, params, details=None, recurse=True):
         """
         This is the main method that is used to send the URL via the request.
         After that it will publish to all subscribers the result.
@@ -139,16 +174,8 @@ class PandaX(ApplicationSession):
         :param recurse: 
         :return: 
         """
-        # print('jsonrpc')
-        # print(params)
-        # print(PandaXAuthenticator.get_auth_token())
-        # print(self.cookies)
-        # print(self.user_sessions)
         token = PandaXAuthenticator.get_auth_token()
-        topic_ids = self.topic_ids[details.procedure]
-        # print(topic_ids)
         is_logged_in = PandaXAuthenticator.is_logged_in(self.cookies)
-        # print(is_logged_in)
         response = None
 
         if is_logged_in or params['allow_anonymous']:
@@ -159,7 +186,6 @@ class PandaX(ApplicationSession):
             }
             payload = {
                 "params": params,
-                'publish': publish,
                 "jsonrpc": "2.0",
                 "id": 0,
             }
@@ -174,9 +200,11 @@ class PandaX(ApplicationSession):
                     raise ApplicationError(u'call.rest.error.authenticate',
                                            'could not authenticate session')
 
-                return self.jsonrpc(self, url, method, params, publish, details, False)
+                return self.jsonrpc(self, url, method, params, details, False)
 
             if response and 'publish' in response:
                 self.publish(procedure, response)
+                return response
 
-        return response
+        return True
+
